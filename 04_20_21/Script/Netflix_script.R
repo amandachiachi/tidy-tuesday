2511371078ddeca19a4b35636b2165aa9b46d86f
Amanda Chiach <amandachiachi@gmail.com>
2021-04-26 08:42:48 -0700

    adding notes for 04_20_21 tidy tuesday

--- 04_20_21/Script/Netflix_script.R
+++ 04_20_21/Script/Netflix_script.R
@@ -11,34 +11,31 @@ library(lubridate)
 here()
 
 #Upload Data####
-tuesdata <- tidytuesdayR::tt_load('2021-04-20')
-netflix <- tuesdata$netflix
+tuesdata <- tidytuesdayR::tt_load('2021-04-20') # load in the data
+netflix <- tuesdata$netflix # parse out what data I want to use
 
-pal <- PNWColors::("BottleRocket1", 6, type = "discrete")
-
-?pivot_wider
-netflix_clean <- netflix %>%
-  select(type, release_year, date_added)%>%
-  separate(date_added, #separate year from month and year
+netflix_clean <- netflix %>% 
+  select(type, release_year, date_added)%>% # select the columns that he wanted to use 
+  separate(date_added, #separate the date into different columns to see what I want to work with
            into = c("Month.Day", "Year"),
-           sep = ", ", remove = TRUE)%>%
-  select(-Month.Day, -release_year)%>%
-  group_by(type, Year)%>%
-  summarise(type_count = n())%>%
+           sep = ", ", remove = TRUE)%>% # remove the extra column
+  select(-Month.Day, -release_year)%>% # remove two columns
+  group_by(type, Year)%>% # group by type and year
+  summarise(type_count = n())%>% # make a new column adding up each entry for type_count
 
-view(netflix_clean)
+view(netflix_clean) # view the data 
 
 
 netflix_clean%>%
-  ggplot(aes(x = Year, 
-             y = type_count, 
-             color = Year))+
-  geom_boxplot(show.legend = FALSE)+
-  labs(title = "Number of Netflix Relseases per Year",
-       x = "Year", 
-       y = "Number of Movie or TV Relseases each Year", 
-       caption = "data from rfordatascience/tidytuesday")+
-  theme_light()+
+  ggplot(aes(x = Year, # x axis will show the year
+             y = type_count, # y axis will show the number of releases each year
+             color = Year))+ # make each Year a different color
+  geom_boxplot(show.legend = FALSE)+ # remove the legend 
+  labs(title = "Number of Netflix Relseases per Year", # title whole plot
+       x = "Year",  # title x axis
+       y = "Number of Movie or TV Relseases each Year", # title y axis 
+       caption = "data from rfordatascience/tidytuesday")+ # give it a caption
+  theme_light()+ # make the theme clean
   scale_y_continuous()+ # make sure y axis is continuous
   theme(axis.title = element_text(size = 15), # size of plot axis title font
         axis.text.x = element_text(angle = 45, vjust = 0.5, hjust=.5), # make plot titles vertical 
