3733f2d160e2c716f58ddf4342606af897f8a834
Amanda Chiach <amandachiachi@gmail.com>
2021-05-03 07:50:49 -0700

    Tidy Tuesday 4_27

---
+++ Tidy_04_27.R
@@ -0,0 +1,12 @@
+# Tidy Tuesday 04_27_21
+
+#Load Libraries####
+library(tidytuesdayR)
+library(tidyverse)
+library(here)
+library(dplyr)
+library(PNWColors)
+library(lubridate)
+
+
+tuesdata <- tidytuesdayR::tt_load('2021-04-27')
